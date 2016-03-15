33bfcbcb16cb707ebdf1cccd3e76d27d5785c032
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-03-15 18:43:14 -0500

    Async config + add imagery and css

--- browser/main.js
+++ browser/main.js
@@ -1,5 +1,5 @@
-var remote = require('remote')
-var path = require('path')
+var insertCss = require('insert-css')
+var userConfig = require('remote').require('./lib/user-config')
 
 var prevhash = localStorage.getItem('location')
 if (location.hash) localStorage.setItem('location', location.hash)
@@ -11,19 +11,31 @@ window.addEventListener('hashchange', function (ev) {
 
 var serverUrl = 'http://' + require('remote').getGlobal('osmServerHost')
 
-var customPresets = remote.require('./lib/user-config').getPresets()
-var customImagery = remote.require('./lib/user-config').getImagery()
-
-var presets = customPresets || require('../vendor/iD/presets.json')
-var imagery = customImagery || require('../vendor/iD/imagery.json')
-
-id = iD()
-  .presets(presets)
-  .imagery(imagery)
+var id = iD()
+  .presets(require('../vendor/iD/presets.json'))
+  .imagery(require('../vendor/iD/imagery.json'))
   .taginfo(iD.services.taginfo())
   .assetPath('vendor/iD/')
   .preauth({url: serverUrl})
   .minEditableZoom(14)
 
-d3.select('#container')
-  .call(id.ui())
+var pending = 3
+
+userConfig.getPresets(function (err, presets) {
+  if (!err) id.presets(presets)
+  done()
+})
+userConfig.getImagery(function (err, imagery) {
+  if (!err) id.imagery(imagery)
+  done()
+})
+userConfig.getCustomCss(function (err, customCss) {
+  if (!err) insertCss(customCss)
+  done()
+})
+
+function done () {
+  if (--pending !== 0) return
+  d3.select('#container')
+    .call(id.ui())
+}
