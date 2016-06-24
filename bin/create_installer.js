8ea9954c14285e14d1f2febea6f3d5ef48af3215
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-06-24 16:42:15 -0700

    Windows installer updates

--- bin/create_installer.js
+++ bin/create_installer.js
@@ -1,11 +1,13 @@
 var electronInstaller = require('electron-winstaller')
 var mkdirp = require('mkdirp').sync
+var rimraf = require('rimraf').sync
 var path = require('path')
 
 var distFolder = path.join(__dirname, '..', 'dist')
 var buildFolder = path.join(distFolder, 'Mapeo-win32-x64')
 var installerFolder = path.join(distFolder, 'installer-win-x64')
 
+rimraf(installerFolder)
 mkdirp(installerFolder)
 
 electronInstaller.createWindowsInstaller({
@@ -13,7 +15,9 @@ electronInstaller.createWindowsInstaller({
   outputDirectory: installerFolder,
   description: 'Offline Map Editor',
   authors: 'Digital Democracy',
-  exe: 'Mapeo.exe'
+  exe: 'Mapeo.exe',
+  setupExe: 'InstallarMapeo.exe',
+  iconUrl: 'https://raw.githubusercontent.com/digidem/mapeo-desktop/master/static/mapeo.ico'
 }).then(function () {
   console.log('It worked!')
 }).catch(function (e) {
