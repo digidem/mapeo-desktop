e2e19022e736c11d8f4bd27e8d23c96b0ac6aee3
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-08-17 09:44:10 -0700

    Import settings from settings file

--- browser/main.js
+++ browser/main.js
@@ -1,6 +1,10 @@
 var insertCss = require('insert-css')
 var ipc = require('electron').ipcRenderer
 var remote = require('electron').remote
+var merge = require('lodash/merge')
+
+var defaultPresets = require('../vendor/iD/presets.json')
+var defaultImagery = require('../vendor/iD/imagery.json')
 
 var prevhash = localStorage.getItem('location')
 if (location.hash) localStorage.setItem('location', location.hash)
@@ -12,36 +16,44 @@ window.addEventListener('hashchange', function (ev) {
 
 var serverUrl = 'http://' + remote.getGlobal('osmServerHost')
 
-var presets = ipc.sendSync('get-user-data', 'presets')
-var customCss = ipc.sendSync('get-user-data', 'css')
-var imagery = ipc.sendSync('get-user-data', 'imagery')
-
-if (customCss) insertCss(customCss)
-
 var id = iD()
-  .presets(presets || require('../vendor/iD/presets.json'))
-  .imagery(imagery || require('../vendor/iD/imagery.json'))
   .taginfo(iD.services.taginfo())
   .assetPath('vendor/iD/')
   .preauth({url: serverUrl})
   .minEditableZoom(14)
 
-ipc.on('updated-user-data', function (event, type, data) {
-  switch (type) {
-    case 'presets':
-      id.presets(data)
-      break
-    case 'css':
-      insertCss(data)
-      break
-    case 'imagery':
-    console.log(data)
-      id.imagery(data)
-      break
-    default:
-      console.warn('unhandled event', event, type)
+id.loadLocale = function(cb) {
+  var locale = iD.detect().locale
+  if (locale && iD.data.locales.indexOf(locale) === -1) {
+    locale = locale.split('-')[0]
   }
-})
+  if (locale && locale !== 'en' && iD.data.locales.indexOf(locale) !== -1) {
+    var localePath = id.asset('locales/' + locale + '.json')
+    d3.json(localePath, function (err, result) {
+      window.locale[locale] = result
+      window.locale.current(locale)
+      var translations = ipc.sendSync('get-user-data', 'translations')
+      merge(window.locale, translations)
+      cb()
+    })
+  } else {
+    cb()
+  }
+}
 
 d3.select('#container')
   .call(id.ui())
+
+updateSettings()
+ipc.on('updated-settings', updateSettings)
+
+function updateSettings () {
+  var presets = ipc.sendSync('get-user-data', 'presets')
+  var customCss = ipc.sendSync('get-user-data', 'css')
+  var imagery = ipc.sendSync('get-user-data', 'imagery')
+  var translations = ipc.sendSync('get-user-data', 'translations')
+  if (customCss) insertCss(customCss)
+  if (translations) merge(window.locale, translations)
+  id.presets(presets || defaultPresets)
+  id.imagery(imagery || defaultPresets)
+}
