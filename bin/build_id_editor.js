85d5d0e5b20eeec9e92a0512558572210f8301ac
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-10-05 12:37:05 -0400

    Iceboxxify build scripts.

--- bin/build_id_editor.js
+++ bin/build_id_editor.js
@@ -3,47 +3,56 @@ var path = require('path')
 var concat = require('@gmaclennan/concat')
 var mkdirp = require('mkdirp')
 var EOL = require('os').EOL
+var icebox = require('ice-box')()
 
 var mpPath = path.resolve(__dirname, '../id_monkey_patches')
 var idPath = path.dirname(require.resolve('iD/package.json'))
 var pkg = require('../package.json')
 var idDistPath = path.join(idPath, 'dist')
-var dstPath = path.resolve(__dirname, '../vendor/iD')
 
-mkdirp.sync(dstPath)
+icebox(function (dstPath, done) {
+  // var dstPath = path.resolve(__dirname, '../vendor/iD')
 
-// Copy all iD dist assets
-fs.copySync(idDistPath, dstPath, {clobber: true})
-fs.copySync(path.join(idPath, 'data/imagery.json'), path.join(dstPath, 'imagery.json'), {clobber: true})
+  mkdirp.sync(dstPath)
 
-var patchPath = path.join(dstPath, 'iD-patched.js')
+  // Copy all iD dist assets
+  fs.copySync(idDistPath, dstPath, {clobber: true})
+  fs.copySync(path.join(idPath, 'data/imagery.json'), path.join(dstPath, 'imagery.json'), {clobber: true})
 
-// Monkey patch and build iD
-concat([
-  path.join(mpPath, 'start.js'),
-  path.join(idDistPath, 'iD.js'),
-  path.join(mpPath, 'id-connection.js'),
-  path.join(mpPath, 'id-modes-browse.js'),
-  path.join(mpPath, 'id-modes-save.js'),
-  path.join(mpPath, 'id-ui-account.js'),
-  path.join(mpPath, 'id-svg-tagclasses.js'),
-  path.join(mpPath, 'osm-auth.js'),
-  path.join(mpPath, 'no-slow.js'),
-  path.join(mpPath, 'end.js')
-], patchPath, done)
+  var patchPath = path.join(dstPath, 'iD-patched.js')
 
-function done (err) {
-  if (err) console.error(err, err.stack)
+  // Monkey patch and build iD
+  concat([
+    path.join(mpPath, 'start.js'),
+    path.join(idDistPath, 'iD.js'),
+    path.join(mpPath, 'id-connection.js'),
+    path.join(mpPath, 'id-modes-browse.js'),
+    path.join(mpPath, 'id-modes-save.js'),
+    path.join(mpPath, 'id-ui-account.js'),
+    path.join(mpPath, 'id-svg-tagclasses.js'),
+    path.join(mpPath, 'osm-auth.js'),
+    path.join(mpPath, 'no-slow.js'),
+    path.join(mpPath, 'end.js')
+  ], path.join(patchPath), finished)
 
-  // needs to happen at build-time: version patch
-  fs.writeFileSync(patchPath, fs.readFileSync(patchPath).toString() + EOL + 'iD.version = "' + pkg.version + '"')
+  function finished (err) {
+    if (err) throw err
 
-  var presets = {
-    presets: require('iD/data/presets/presets.json'),
-    defaults: require('iD/data/presets/defaults.json'),
-    categories: require('iD/data/presets/categories.json'),
-    fields: require('iD/data/presets/fields.json')
+    // needs to happen at build-time: version patch
+    fs.writeFileSync(patchPath, fs.readFileSync(patchPath).toString() + EOL + 'iD.version = "' + pkg.version + '"')
+
+    var presets = {
+      presets: require('iD/data/presets/presets.json'),
+      defaults: require('iD/data/presets/defaults.json'),
+      categories: require('iD/data/presets/categories.json'),
+      fields: require('iD/data/presets/fields.json')
+    }
+
+    fs.writeFileSync(path.join(dstPath, 'presets.json'), JSON.stringify(presets, null, '  '))
+
+    done()
   }
-
-  fs.writeFileSync(path.join(dstPath, 'presets.json'), JSON.stringify(presets, null, '  '))
-}
+}, function (err, finalPath) {
+  if (err) throw err
+  console.log(finalPath)
+})
--- bin/create_windows_installer.js
+++ bin/create_windows_installer.js
@@ -2,31 +2,32 @@ var electronInstaller = require('electron-winstaller')
 var mkdirp = require('mkdirp').sync
 var rimraf = require('rimraf').sync
 var path = require('path')
+var icebox = require('ice-box')()
+var arg = require('argv-or-stdin')
 
-var distFolder = path.join(__dirname, '..', 'dist')
 var pkg = require(path.join('..', 'package.json'))
-var buildFolder = path.join(distFolder, 'Mapeo-win32-x64')
-var installerFolder = path.join(distFolder, 'installer-win-x64')
 
-rimraf(installerFolder)
-mkdirp(installerFolder)
+arg(function (err, srcPath) {
+  icebox(function (dstPath, done) {
+    electronInstaller.createWindowsInstaller({
+      appDirectory: path.join(srcPath, 'Mapeo-win32-x64'),
+      outputDirectory: dstPath,
 
-electronInstaller.createWindowsInstaller({
-  appDirectory: buildFolder,
-  outputDirectory: installerFolder,
+      usePackageJson: false,
 
-  usePackageJson: false,
-
-  description: pkg.productDescription,
-  authors: pkg.author,
-  name: 'Mapeo',
-  exe: 'Mapeo.exe',
-  setupExe: 'Installar_Mapeo_' + pkg.version + '_Windows.exe',
-  iconUrl: 'https://raw.githubusercontent.com/digidem/mapeo-desktop/master/static/mapeo.ico',
-  version: pkg.version,
-  title: 'mapeo',
-}).then(function () {
-  console.log('It worked!')
-}).catch(function (e) {
-  console.error(`No dice: ${e.message}`)
+      description: pkg.productDescription,
+      authors: pkg.author,
+      name: 'Mapeo',
+      exe: 'Mapeo.exe',
+      setupExe: 'Installar_Mapeo_' + pkg.version + '_Windows.exe',
+      iconUrl: 'https://raw.githubusercontent.com/digidem/mapeo-desktop/master/static/mapeo.ico',
+      version: pkg.version,
+      title: 'mapeo',
+    })
+    .then(done)
+    .catch(function (e) { done(e) })
+  }, function (err, finalPath) {
+    if (err) return console.trace(err)
+    console.log(finalPath)
+  })
 })
