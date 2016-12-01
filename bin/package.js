77e5a4210f0c723f45fb057ba0c792dbaeac17a0
Stephen Whitmore <sww@eight45.net>
2016-12-01 17:17:39 -0500

    Delete old build before packaging new ones.

--- bin/package.js
+++ bin/package.js
@@ -1,24 +1,37 @@
+var path = require('path')
+var rimraf = require('rimraf')
 var packager = require('electron-packager')
+var config = require('../config')
 
 if (process.argv.length !== 3) {
   printUsageAndDie()
 }
 
-var config = getPlatformConfiguration(process.argv[2])
-if (!config) {
+var cfg = getPlatformConfiguration(process.argv[2])
+if (!cfg) {
   printUsageAndDie()
 }
 
-packager(config, function (err, appPaths) {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  } else if (appPaths.length < 1) {
-    throw new Error('expected appPaths to have >= 1 entry')
-  } else {
-    console.log(appPaths[0])
-  }
+var distFolder = path.join(config.ROOT_PATH, 'dist')
+var buildName = 'mapfilter-desktop-' + cfg.platform + '-' + cfg.arch
+var buildFolder = path.join(distFolder, buildName)
+rimraf(buildFolder, function (err) {
+  if (err) throw err
+  doPackaging(cfg)
 })
+
+function doPackaging (cfg) {
+  packager(cfg, function (err, appPaths) {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    } else if (appPaths.length < 1) {
+      throw new Error('expected appPaths to have >= 1 entry')
+    } else {
+      console.log(appPaths[0])
+    }
+  })
+}
 
 function printUsageAndDie () {
   console.error('USAGE: package.js [windows|macos]')
