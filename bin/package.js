a341ea62f998f2849be47c5f91633cb7bee9ea97
Stephen Whitmore <sww@eight45.net>
2016-12-01 17:04:34 -0500

    Refactor bin/package.js.

--- bin/package.js
+++ bin/package.js
@@ -4,12 +4,8 @@ if (process.argv.length !== 3) {
   printUsageAndDie()
 }
 
-var config
-if (process.argv[2] === 'windows') {
-  config = require('./windows_config.js')
-} else if (process.argv[2] === 'macos') {
-  config = require('./macos_config.js')
-} else {
+var config = getPlatformConfiguration(process.argv[2])
+if (!config) {
   printUsageAndDie()
 }
 
@@ -17,6 +13,8 @@ packager(config, function (err, appPaths) {
   if (err) {
     console.error(err)
     process.exit(1)
+  } else if (appPaths.length < 1) {
+    throw new Error('expected appPaths to have >= 1 entry')
   } else {
     console.log(appPaths[0])
   }
@@ -25,4 +23,14 @@ packager(config, function (err, appPaths) {
 function printUsageAndDie () {
   console.error('USAGE: package.js [windows|macos]')
   process.exit(1)
+}
+
+function getPlatformConfiguration (platform) {
+  if (platform === 'windows') {
+    return require('./windows_config.js')
+  } else if (platform === 'macos') {
+    return require('./macos_config.js')
+  } else {
+    return null
+  }
 }
