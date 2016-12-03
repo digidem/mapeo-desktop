2a15dfcf2e2af8b59e60c7f4ef9cf708c30ec67b
Stephen Whitmore <sww@eight45.net>
2016-12-03 15:34:50 -0500

    Add Electron debug key bindings by default.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var app = electron.app  // Module to control application life.
 var Menu = electron.Menu
 var BrowserWindow = electron.BrowserWindow  // Module to create native browser window.
 
+require('electron-debug')({showDevTools: false})
+
 // Path to `userData`, operating system specific, see
 // https://github.com/atom/electron/blob/master/docs/api/app.md#appgetpathname
 var userDataPath = app.getPath('userData')
