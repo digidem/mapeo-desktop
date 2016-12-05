45db44f4bfce53e914a0a4edcdee8d0386ff43eb
Stephen Whitmore <sww@eight45.net>
2016-12-04 22:36:33 -0500

    Revert "TEMP: some hacky sqlite3 code to see if it works cross-platform."
    This reverts commit 738e2ffe17ef6bd44411f17bdba23bbc19d53e5b.

--- app.js
+++ app.js
@@ -13,26 +13,6 @@ var osmdb = require('osm-p2p')
 var obsdb = require('osm-p2p-observations')
 var level = require('level')
 
-var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database(':memory:')
-
-db.serialize(function () {
-  db.run('CREATE TABLE lorem (info TEXT)')
-
-  var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-  for (var i = 0; i < 10; i++) {
-    stmt.run('Ipsum ' + i)
-  }
-  stmt.finalize()
-
-  db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
-    if (err) console.error(err)
-    else console.log(row.id + ': ' + row.info)
-  })
-})
-
-// db.close();
-
 require('electron-debug')({showDevTools: true})
 
 // Path to `userData`, operating system specific, see
