9f55a44a875d9cfc5570583d884dc608677c1f01
Seth Fitzsimmons <seth@mojodna.net>
2016-12-12 12:20:27 -0800

    MapFilter component embed
    Browserify is necessary in order to strip types from mapbox-gl, but it
    also means that we can use future JS features without needing to pay
    attention to what Electron supports.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const MapFilter = require('react-mapfilter')
+const fs = require('fs')
+const path = require('path')
+
+const sampleGeoJSON = fs.readFileSync(path.join(__dirname, './sample.geojson'), 'utf8')
+const features = JSON.parse(sampleGeoJSON).features
+
+ReactDOM.render(React.createElement(MapFilter, {features: features}), document.getElementById('root'))
