fbbe6fb6fcbb296408fc56f95f8e064fe70de3b5
Seth Fitzsimmons <seth@mojodna.net>
2016-12-12 13:00:58 -0800

    Strip babel deps

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+require('babel-polyfill')
+
+const fs = require('fs')
+const path = require('path')
+
 const React = require('react')
 const ReactDOM = require('react-dom')
 const MapFilter = require('react-mapfilter')
-const fs = require('fs')
-const path = require('path')
 
 const sampleGeoJSON = fs.readFileSync(path.join(__dirname, './sample.geojson'), 'utf8')
 const features = JSON.parse(sampleGeoJSON).features
