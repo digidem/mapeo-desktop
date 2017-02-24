9bc228d219c7abdc979d88cfa680ef13f258ab6a
Stephen Whitmore <sww@eight45.net>
2017-02-23 16:51:32 -0800

    wip

--- browser/main.js
+++ browser/main.js
@@ -81,6 +81,10 @@ function updateSettings () {
   var customCss = ipc.sendSync('get-user-data', 'css')
   var imagery = ipc.sendSync('get-user-data', 'imagery')
   var translations = ipc.sendSync('get-user-data', 'translations')
+
+  // TODO: where is this saved to? loaded at?
+  var layers = ipc.sendSync('get-user-data', 'layers')
+
   var icons = ipc.sendSync('get-user-data', 'icons')
   if (icons) {
     var iconsSvg = parser.parseFromString(icons, 'image/svg+xml').documentElement
