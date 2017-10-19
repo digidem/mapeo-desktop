0cca0a88dff25bddcd37ba32ada5ce36b52b5e36
karissa <krmckelv@gmail.com>
2017-10-19 00:01:00 -0700

    set up welcome page for translation

--- browser/welcome.js
+++ browser/welcome.js
@@ -1,7 +1,9 @@
 var ipc = require('electron').ipcRenderer
 var path = require('path')
+var i18n = require('../lib/i18n')
 
 module.exports = function ($overlay, $welcome, $map) {
+  $welcome.innerHTML = render()
   $welcome.style = ''
   $overlay.style = 'visibility: hidden;'
   $map.style = 'visibility: hidden;'
@@ -33,4 +35,45 @@ module.exports = function ($overlay, $welcome, $map) {
   function openMap (ev) {
     window.location.href = 'index.html'
   }
+}
+
+function render () {
+  return `
+    <div id="skip-intro">Skip intro</div>
+    <div id="screen-1">
+      <img src="static/Dd-square-solid-300.png" />
+      <h1>${i18n('welcome-screen-1-title')}</h1>
+      <p>${i18n('welcome-screen-1-subtitle')}</p>
+      <button class="big-button next-screen" data-next="screen-2">
+        ${i18n('welcome-screen-1-next-button')}
+      </button>
+    </div>
+    <div id="screen-2" style="display: none;">
+      <h1>
+        ${i18n('welcome-screen-2-title')}
+      </h1>
+      <h2 class="intro-text">${i18n('welcome-screen-2-text-1')}</h2>
+      <p>${i18n('welcome-screen-2-text-2')}</p>
+      <p>${i18n('welcome-screen-2-text-3')}</p>
+      <p>${i18n('welcome-screen-2-text-4')}</p>
+      <p>${i18n('welcome-screen-2-text-5')}</p>
+      <button class="big-button next-screen" data-next="screen-3">
+        ${i18n('welcome-screen-2-next-button')}
+      </button>
+    </div>
+    <div id="screen-3" style="display: none;">
+      <h2 class="intro-text">${i18n('welcome-screen-3-title')}</h2>
+      <div class="action-buttons">
+        <button id="example-dataset" class="big-button">
+        ${i18n('welcome-screen-3-example-dataset')}
+        </button>
+        <button id="use-presets" class="big-button">
+        ${i18n('welcome-screen-3-use-presets')}
+        </button>
+        <button id="open-map" class="big-button">
+        ${i18n('welcome-screen-3-open-map')}
+        </button>
+      </div>
+    </div>
+  `
 }
