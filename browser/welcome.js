8ed06a691ef6f40a53b64f24cdb3d981c3c17a94
karissa <krmckelv@gmail.com>
2017-10-20 09:53:50 -0700

    add links to welcome page

--- browser/welcome.js
+++ browser/welcome.js
@@ -56,7 +56,9 @@ function render () {
       <p>${i18n('welcome-screen-2-text-2')}</p>
       <p>${i18n('welcome-screen-2-text-3')}</p>
       <p>${i18n('welcome-screen-2-text-4')}</p>
-      <p>${i18n('welcome-screen-2-text-5')}</p>
+      <p>This is a preview of Mapeo, it is in continued development and we are building an accompanying mobile app.
+      <a href="https://mapeo.world">Sign up on our website</a> to receive notifications when new versions are released.
+      <p>We welcome your feedback and reporting of bugs, please get in touch through our <a href="https://github.com/digidem/mapeo-desktop">GitHub repository</a>.</p>
       <button class="big-button next-screen" data-next="screen-3">
         ${i18n('welcome-screen-2-next-button')}
       </button>
