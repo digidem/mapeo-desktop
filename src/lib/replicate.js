e06c2945551ccd2fa394fb4ef2f65468418acab0
karissa <krmckelv@gmail.com>
2018-06-08 14:09:12 -0700

    use HTTP GET streams for replication status

--- src/lib/replicate.js
+++ src/lib/replicate.js
@@ -1,12 +1,12 @@
-import through from 'through2'
-import split from 'split2'
-import wsock from 'websocket-stream'
+import xhr from 'xhr'
+import querystring from 'querystring'
+import split2 from 'split2'
 import pump from 'pump'
 import {remote} from 'electron'
-import xhr from 'xhr'
+import hyperquest from 'hyperquest'
 
 module.exports = {
-  start, parseMessages, getTargets
+  start, getTargets
 }
 
 function getTargets (cb) {
@@ -15,34 +15,15 @@ function getTargets (cb) {
     method: 'GET',
     url: 'http://' + osmServerHost + '/sync/targets'
   }
-  _req(opts, cb)
-}
-
-function start (target, cb) {
-  var osmServerHost = remote.getGlobal('osmServerHost')
-  var opts = {
-    method: 'POST',
-    url: 'http://' + osmServerHost + '/sync/start',
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify(target)
-  }
-  _req(opts, cb)
-}
-
-function parseMessages (cb) {
-  var osmServerHost = remote.getGlobal('osmServerHost')
-  var ws = wsock('ws://' + osmServerHost)
-
-  return pump(ws, split(JSON.parse), through.obj(function (row, enc, next) {
-    if (row) cb(row, next)
-  })).on('error', cb)
-}
-
-function _req (opts, cb) {
   xhr(opts, function (err, res, body) {
     if (err) return cb(err)
     return cb(null, body)
   })
+}
+
+function start (target, cb) {
+  var osmServerHost = remote.getGlobal('osmServerHost')
+  var url = `http://${osmServerHost}/sync/start?${querystring.stringify(target)}`
+  var hq = hyperquest(url)
+  return pump(hq, split2())
 }
