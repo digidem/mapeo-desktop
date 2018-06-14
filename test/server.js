1c29a41fdd83cd97c2520dceb0f4a3cca80b79f5
karissa <krmckelv@gmail.com>
2018-06-14 13:51:12 -0400

    add test server

---
+++ test/server.js
@@ -0,0 +1,29 @@
+var mkdirp = require('mkdirp')
+var rimraf = require('rimraf')
+var path = require('path')
+var Mapeo = require('mapeo-server')
+var blobstore = require('safe-fs-blob-store')
+var osmdb = require('osm-p2p')
+var http = require('http')
+
+var dir = path.join(__dirname, 'test-data')
+var osm = osmdb(path.join(dir, 'osm'))
+var media = blobstore(path.join(dir, 'media'))
+
+var port = 5006
+
+mkdirp.sync(dir)
+var ws = media.createWriteStream('file.txt')
+ws.write('hi')
+ws.end()
+var mapeo = Mapeo(osm, media)
+var server = http.createServer(function (req, res) {
+  if (!mapeo(req, res)) {
+    res.statusCode = 404
+    res.end('404')
+  }
+})
+
+server.listen(port, function () {
+  console.log('listening on port', port)
+})
