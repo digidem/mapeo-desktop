cbfdaad99aed08cfbb05925f6e07178a4994634b
karissa <krmckelv@gmail.com>
2018-06-15 10:43:23 -0700

    update to mapeo-server@3.0.0

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
+var path = require('path')
 var mapeoserver = require('mapeo-server')
 var osmserver = require('osm-p2p-server')
 var http = require('http')
 
 module.exports = function (osm, media) {
   var osmrouter = osmserver(osm)
-  var mapeo = mapeoserver(osm, media)
+  var root = path.basename(require.resolve('mapeo-styles'))
+  var mapeo = mapeoserver(osm, media, {root})
 
   var server = http.createServer(function (req, res) {
-    var m = osmrouter.handle(req, res) || mapeo(req, res)
+    var m = osmrouter.handle(req, res) || mapeo.handle(req, res)
     if (!m) {
       res.statusCode = 404
       res.end('Not Found')
     }
   })
-  server.sync = mapeo.sync
+  server.mapeo = mapeo
   return server
 }
