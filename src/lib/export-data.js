5cf66da3cd0e40b0a9e59147e447a62af7c627e8
karissa <krmckelv@gmail.com>
2018-06-18 16:16:01 -0700

    export mapeodata from file menu

--- src/lib/export-data.js
+++ src/lib/export-data.js
@@ -1,14 +1,22 @@
 var path = require('path')
 var pump = require('pump')
 var fs = require('fs')
+var replicate = require('./replicate')
 var exportGeoJson = require('./export-geojson')
 var exportShapefile = require('./export-shapefile')
 
-module.exports = function (osm, filename, done) {
+module.exports = function (mapeo, filename, done) {
   var ext = path.extname(filename)
   switch (ext) {
-    case '.geojson': return pump(exportGeoJson(osm), fs.createWriteStream(filename), done)
-    case '.shp': return exportShapefile(osm, filename, done)
+    case '.geojson': return pump(exportGeoJson(mapeo.api.osm), fs.createWriteStream(filename), done)
+    case '.shp': return exportShapefile(mapeo.api.osm, filename, done)
+    case '.mapeodata': return exportSyncfile(mapeo.api.sync, filename, done)
     default: return done(new Error('Extension not supported'))
   }
+}
+
+function exportSyncfile (sync, filename, done) {
+  var progress = sync.replicateFromFile(filename)
+  progress.on('error', done)
+  progress.on('end', done)
 }
