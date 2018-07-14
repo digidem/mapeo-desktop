9ef6a727b13ed9216e61291ada05a7ffb0d88667
karissa <krmckelv@gmail.com>
2018-07-14 09:15:36 +0200

    simplify sync view rendering

--- src/components/SyncView.js
+++ src/components/SyncView.js
@@ -3,7 +3,6 @@ import React from 'react'
 import randombytes from 'randombytes'
 import {ipcRenderer, remote} from 'electron'
 
-import SyncComponent from 'mapeo-components'
 import Modal from './Modal'
 import Form from './Form'
 import i18n from '../lib/i18n'
@@ -71,9 +70,38 @@ export default class SyncView extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      filename: props.filename
+      targets: {}
     }
     this.selectFile = this.selectFile.bind(this)
+  }
+
+  replicate (target) {
+    var self = this
+    if (!target) return
+    var stream = replicate.start(target)
+    var id = randombytes(16).toString('hex')
+    this.streams[id] = stream
+    stream.on('data', function (data) {
+      try {
+        var row = JSON.parse(data)
+      } catch (err) {
+        console.error(err)
+        return
+      }
+      var t = Target(target)
+      t.status = row.topic
+      t.message = self.props.messages[row.topic] || row.message
+      self.state.targets[target.name] = t
+      if (status !== 'replication-progress') self.setState({statuses: self.state.statuses})
+    })
+
+    stream.on('error', function (err) {
+      if (err) console.error(err)
+    })
+
+    stream.on('end', function () {
+      delete self.streams[id]
+    })
   }
 
   componentWillUnmount () {
@@ -89,9 +117,21 @@ export default class SyncView extends React.Component {
     ipcRenderer.send('refresh-window')
   }
 
-  selectFile (event, filename) {
-    this.setState({filename})
-  }
+  updateTargets () {
+    var self = this
+    getTargets(this.props.server, function (err, targets) {
+      if (err) return console.error(err)
+      targets = JSON.parse(targets)
+      Object.keys(self.state.targets).forEach(function (name) {
+        var match = targets.filter((t) => t.name === name).length
+        if (!match) delete self.state.targets[name]
+      })
+      targets.forEach(function (t) {
+        var old = self.state.targets[t.name] || {}
+        self.state.targets[t.name] = Target(Object.assign(old, t))
+      })
+      self.setState({targets: self.state.targets})
+    })}
 
   selectExisting (event) {
     event.preventDefault()
@@ -105,34 +145,59 @@ export default class SyncView extends React.Component {
 
   render () {
     var self = this
-    const {filename} = this.state
-    const onClose = this.onClose.bind(this)
-    const server = 'http://' + remote.getGlobal('osmServerHost')
+    var {targets} = this.state
+    if (this.props.filename) this.selectFile(this.props.filename)
+    var disabled = Object.keys(self.streams).length > 0
+    var onClose = this.onClose.bind(this)
 
     return (
       <Modal closeButton={false} onClose={onClose} title={i18n('sync-database-lead')}>
         <TargetsDiv>
-          <SyncComponent messages={messages} server={server} filename={filename} />
+        {Object.keys(targets).length === 0
+          ? <Subtitle>Searching for devices&hellip;</Subtitle>
+          : <Subtitle>Available Devices</Subtitle>
+        }
+          <ul>
+            {Object.keys(targets).map(function (key) {
+              var t = targets[key]
+              if (t.name === 'localhost') return
+              return (
+                <Target key={t.name}>
+                  <div className='target'>
+                    <span className='name'>{t.name}</span>
+                    <span className='info'>via {t.type}</span>
+                  </div>
+                  {t.status ? <h3>{t.message}</h3> :
+                    <SyncButton onClick={self.replicate.bind(self, t)}>
+                      arrow
+                    </SyncButton>
+                  }
+                </Target>
+              )
+            })}
+          </ul>
         </TargetsDiv>
-        <Form method='POST'>
-          <input type='hidden' name='source' />
-          <div className='button-group'>
-            <button className='big' onClick={this.selectExisting}>
-              <span id='button-text'>
-                {i18n('sync-database-open-button')}&hellip;
-              </span>
-            </button>
-            <button className='big' onClick={this.selectNew}>
-              <span id='button-text'>
-                {i18n('sync-database-new-button')}&hellip;
-              </span>
-            </button>
-            <button className='big' onClick={onClose}>
-              Done
-            </button>
-          </div>
-        </Form>
-      </Modal>
+          <Form method='POST'>
+            <input type='hidden' name='source' />
+            <div className='button-group'>
+              <button className='big' onClick={this.selectExisting}>
+                <span id='button-text'>
+                  {i18n('sync-database-open-button')}&hellip;
+                </span>
+              </button>
+              <button className='big' onClick={this.selectNew}>
+                <span id='button-text'>
+                  {i18n('sync-database-new-button')}&hellip;
+                </span>
+              </button>
+              <button
+                className='big' onClick={onClose}
+                disabled={disabled}>
+                {disabled ? 'Please wait...' : 'Done'}
+              </button>
+            </div>
+          </Form>
+        </Modal>
     )
   }
 }
