1c32dabeeba627879136be4dfd11ca8c11e7f50c
Karissa McKelvey <krmckelv@gmail.com>
2018-09-12 13:12:23 -0700

    handle errors within esctatic after mapeo server and osm-p2p-server have been called

--- src/server.js
+++ src/server.js
@@ -12,18 +12,17 @@ module.exports = function (osm, media) {
   var server = http.createServer(function (req, res) {
     var staticHandler = ecstatic({
       root: path.join(__dirname, '..', 'static'),
-      baseDir: 'static',
-      handleError: false
+      baseDir: 'static'
     })
 
-    function next () {
-      var m = osmrouter.handle(req, res) || mapeo.handle(req, res)
-      if (!m) {
+    var m = osmrouter.handle(req, res) || mapeo.handle(req, res)
+    if (!m) {
+      staticHandler(req, res, function (err) {
+        if (err) console.error(err)
         res.statusCode = 404
         res.end('Not Found')
-      }
+      })
     }
-    staticHandler(req, res, next)
   })
   server.mapeo = mapeo
   return server
