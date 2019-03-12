digidem/mapeo-desktop

commit 1f055b46c82e0056eba5d8eda6ec6277d97dc579

don't refresh the window, just flush iD editor

src/components/MapEditor.js
@@ -15,9 +15,9 @@ export default class MapEditor extends React.Component {
     ipcRenderer.on('zoom-to-data-response', self.zoomToDataResponse.bind(self))
     ipcRenderer.on('zoom-to-latlon-response', self.zoomToLatLonResponse.bind(self))
     ipcRenderer.on('change-language-request', self.changeLanguageRequest.bind(self))
+    ipcRenderer.on('refresh-window', self.refreshWindow.bind(self))
     ipcRenderer.on('updated-settings', function () {
       self.updateSettings()
-      ipcRenderer.send('refresh-window')
     })
   }
 
@@ -27,6 +27,10 @@ export default class MapEditor extends React.Component {
         <div id='container' />
       </div>
     )
+  }
+
+  refreshWindow () {
+    if (this.id) this.id.flush()
   }
 
   componentDidMount () {

index.js
@@ -328,7 +328,7 @@ function createMainWindow (done) {
     })
 
     ipc.on('refresh-window', function () {
-      win.reload()
+      win.webContents.send('refresh-window')
     })
 
     var menu = Menu.buildFromTemplate(menuTemplate(app))

commit 0a13dc5674fd647bf7343f58f1e5eb9b7b2a9ea3

loading presets/settings requires refreshing ui of id editor entirely. this bug was introduced in 1f055b4

src/components/MapEditor.js
@@ -38,7 +38,7 @@ export default class MapEditor extends React.Component {
     ipcRenderer.on('change-language-request', self.changeLanguageRequest)
     ipcRenderer.on('refresh-window', self.refreshWindow)
     ipcRenderer.on('updated-settings', function () {
-      self.updateSettings()
+      window.location.reload()
     })
   }
 