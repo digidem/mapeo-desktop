2bcd7bec4813dced8cb4acd8906bf93653727ece
Karissa McKelvey <krmckelv@gmail.com>
2019-04-03 16:34:32 -0600

    add conversion script for hyperlog

---
+++ bin/convert.js
@@ -0,0 +1,111 @@
+var hyperlog = require('hyperlog')
+var level = require('level')
+var Osmdb = require('osm-p2p')
+var Mapeo = require('mapeo-core')
+var geojson = require('osm-p2p-geojson')
+var exportGeojson = require('./src/lib/export-geojson')
+var collect = require('collect-stream')
+
+/* converts data from hyperlog to hypercore */
+
+module.exports = convert
+function convert (input, output) {
+  console.log(input, output)
+  var osm = Osmdb(output)
+  var mapeo = new Mapeo(osm)
+  var log = hyperlog(level(input), {valueEncoding: 'json'})
+  var rs = log.createReadStream()
+  rs.on('data', function (data) {
+    var val = data.value.v
+    if (val && val.type === 'observation') {
+      mapeo.observationCreate(transformObservationSchema1(val), function (err) {
+        if (err) throw err
+      })
+    }
+  })
+  rs.on('error', function (err) {
+    if (err) throw err
+  })
+  rs.on('end', function () {
+    console.log('adding osm data')
+    var stream = exportGeojson(osm)
+    collect(stream, function (err, data) {
+      if (err) throw err
+      var fc = JSON.parse(data)
+      console.log('Begin importing fc', fc)
+      var importer = geojson.importer(osm)
+      importer.importFeatureCollection(fc)
+      importer.on('import', function (index, length) {
+        console.log(`imported ${index}/${length}`)
+      })
+      importer.on('error', function (err) {
+        if (err) throw err
+      })
+      importer.on('end', function () {
+        console.log('done adding osm data')
+      })
+    })
+  })
+}
+
+// Top-level props that can be modified by the user/client
+var USER_UPDATABLE_PROPS = [
+  'lon',
+  'lat',
+  'attachments',
+  'tags',
+  'ref',
+  'metadata',
+  'fields',
+  'schemaVersion'
+]
+
+// All valid top-level props
+var TOP_LEVEL_PROPS = USER_UPDATABLE_PROPS.concat([
+  'created_at',
+  'timestamp',
+  'id',
+  'version',
+  'type'
+])
+
+var SKIP_OLD_PROPS = [
+  'created_at_timestamp',
+  'link',
+  'device_id',
+  'observedBy'
+]
+
+// Transform an observation from Sinangoe version of MM to the current format
+function transformObservationSchema1 (obs) {
+  var newObs = { tags: {} }
+  Object.keys(obs).forEach(function (prop) {
+    if (prop === 'attachments') {
+      // Attachments has changed from array of strings to array of objects
+      newObs.attachments = (obs.attachments || []).map(a => {
+        if (typeof a !== 'string') return a
+        return { id: a }
+      })
+    } else if (prop === 'fields') {
+      // fields.answer should be a tag
+      newObs.fields = obs.fields || []
+      newObs.fields.forEach(f => {
+        if (!f || !f.answer || !f.id) return
+        newObs.tags[f.id] = f.answer
+      })
+    } else if (SKIP_OLD_PROPS.indexOf(prop) > -1) {
+      // just ignore unused old props
+    } else if (TOP_LEVEL_PROPS.indexOf(prop) > -1) {
+      // Copy across valid top-level props
+      newObs[prop] = obs[prop]
+    } else if (prop === 'created') {
+      // created is changed to created_at
+      newObs.created_at = obs.created
+    } else {
+      newObs.tags[prop] = obs[prop]
+    }
+  })
+  return newObs
+}
+
+convert.apply(null, process.argv.slice(2))
