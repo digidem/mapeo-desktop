dfa014239463e180cd3b99bf2ffee8e6c56c3eb8
Karissa McKelvey <krmckelv@gmail.com>
2019-04-26 00:59:21 +0300

    add clear function for sync state

--- src/renderer/sync-manager.js
+++ src/renderer/sync-manager.js
@@ -1,36 +1,13 @@
 import events from 'events'
 import api from './api'
 
-const WIFI_READY = { topic: 'replication-wifi-ready' }
-
 class SyncManager extends events.EventEmitter {
-  constructor () {
-    super()
-    this.progress = {}
-  }
-
   start (target, opts) {
     if (!target) return this.handleError(new Error('target required, got null'))
     if (!opts) opts = {}
     target.name = target.filename || target.name
-    this.progress[target.name] = {
-      target: target,
-      info: {
-        topic: 'replication-waiting'
-      }
-    }
     this._emitPeers()
-    target.name = target.filename || target.name
-    var interval = opts.interval || 3000
-    var stream = api.start(target, { interval })
-    stream.on('data', (data) => {
-      try {
-        var info = JSON.parse(data.toString())
-      } catch (err) {
-        this.handleError(new Error('Error in json parsing', err))
-      }
-      this.progress[target.name] = { target, info }
-    })
+    api.start(target)
   }
 
   _emitPeers () {
@@ -41,7 +18,7 @@ class SyncManager extends events.EventEmitter {
   }
 
   clearState () {
-    this.progress = {}
+    api.clear(this.handleError)
   }
 
   handleError (err) {
@@ -67,22 +44,7 @@ class SyncManager extends events.EventEmitter {
   }
 
   peers (cb) {
-    api.peers((err, peers) => {
-      if (err) return cb(err)
-      // get wifi peers
-      // make a copy because we are modifying and returning this
-      var state = Object.assign({}, this.progress)
-      peers.map((peer) => {
-        if (!peer.name) return this.handleError(new Error('No name for this peer why? This should never happen.', peer))
-        var progress = this.progress[peer.name]
-        console.log(progress)
-        state[peer.name] = {
-          info: (progress && progress.info) || WIFI_READY,
-          target: peer
-        }
-      })
-      cb(null, Object.values(state))
-    })
+    api.peers(cb)
   }
 }
 
