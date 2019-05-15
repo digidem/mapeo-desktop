8725864e56a53e06b708d4c7d656890137043fc9
Karissa McKelvey <krmckelv@gmail.com>
2019-05-15 19:06:45 +0200

    improve rendering of error in dialog

--- src/renderer/components/dialogs/Error.js
+++ src/renderer/components/dialogs/Error.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Dialog from '@material-ui/core/Dialog'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import TextField from '@material-ui/core/TextField'
 
 export default class LatLonDialog extends React.Component {
   render () {
+    const { message } = this.props
     return (
       <Dialog open onClose={this.props.onClose}>
         <DialogTitle>Error</DialogTitle>
         <DialogContent>
-          {this.props.message} // TODO: escape this html for displaying newlines
+          <TextField style='min-width=400px;' value={message} multiline error disabled />
         </DialogContent>
       </Dialog>
     )
