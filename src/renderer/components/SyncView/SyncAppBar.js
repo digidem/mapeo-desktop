b9c938d982eb3963200b1ccd020b6117579db94f
Thibaut REMY <thibaut@thibaut.re>
2020-03-21 00:06:00 +0100

    feat: add WiFi ssid, remove copy 'via Wi-Fi'

--- src/renderer/components/SyncView/SyncAppBar.js
+++ src/renderer/components/SyncView/SyncAppBar.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
+import { Wifi, WifiOff } from '@material-ui/icons'
 import { defineMessages, useIntl } from 'react-intl'
+import wifi from 'node-wifi'
+import { Tooltip } from '@material-ui/core'
+
+wifi.init({
+  iface: null
+})
 
 const m = defineMessages({
   // Title of sync screen
@@ -18,13 +25,37 @@ const m = defineMessages({
 const SyncAppBar = ({ onClickSelectSyncfile, onClickNewSyncfile }) => {
   const cx = useStyles()
   const { formatMessage: t } = useIntl()
+  const [currentConnection, setCurrentConnection] = useState(null)
+
+  useEffect(() => {
+    wifi
+      .getCurrentConnections()
+      .then((conn) => conn && setCurrentConnection(conn[0]))
+  }, [])
   return (
     <AppBar position='static' color='default' elevation={0} className={cx.root}>
       <Toolbar>
-        <Typography variant='h6' component='h1' className={cx.title}>
-          {t(m.title)}
-          <span className={cx.subtitle}>{' ' + t(m.subtitle)}</span>
-        </Typography>
+        <div className={cx.titleBar}>
+          <Typography variant='h6' component='h1' className={cx.title}>
+            {t(m.title)}
+          </Typography>
+          <span className={cx.wifi}>
+            {currentConnection ? (
+              <Wifi className={cx.wifiIcon} />
+            ) : (
+              <WifiOff className={cx.wifiIcon} />
+            )}
+            {currentConnection && (
+              <Typography
+                variant='overline'
+                component='span'
+                className={cx.wifiName}
+              >
+                {currentConnection.ssid}
+              </Typography>
+            )}
+          </span>
+        </div>
         <Button
           onClick={onClickSelectSyncfile}
           color='inherit'
@@ -48,15 +79,31 @@ const SyncAppBar = ({ onClickSelectSyncfile, onClickNewSyncfile }) => {
 
 export default SyncAppBar
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: 'white'
   },
-  title: {
-    flex: 1
+  titleBar: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start'
   },
-  subtitle: {
-    fontWeight: 300
+  title: {
+    lineHeight: 1.2
+  },
+  wifi: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  wifiIcon: {
+    fontSize: '1rem',
+    position: 'relative',
+    top: -1 // visual alignment
+  },
+  wifiName: {
+    lineHeight: 1.2,
+    marginLeft: 5
   },
   actionArea: {},
   button: {
