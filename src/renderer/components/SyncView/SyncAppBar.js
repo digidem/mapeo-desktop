e61a216d39d956b9150fe0d868b851f2ef9a2874
Thibaut <thibaut@thibaut.re>
2020-03-26 00:11:17 +0100

    Show Wi-Fi SSID (#315)
    * feat: add WiFi ssid, remove copy 'via Wi-Fi'
    
      * performs network check every 2 seconds
      * adds info if network is disconnected
    
    Co-authored-by: Karissa McKelvey <633012+okdistribute@users.noreply.github.com>

--- src/renderer/components/SyncView/SyncAppBar.js
+++ src/renderer/components/SyncView/SyncAppBar.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/core/styles'
+import { Wifi, WifiOff } from '@material-ui/icons'
 import { defineMessages, useIntl } from 'react-intl'
+import wifi from 'node-wifi'
+
+wifi.init({
+  iface: null
+})
 
 const m = defineMessages({
   // Title of sync screen
   title: 'Available Devices',
-  subtitle: 'via Wi-Fi',
+  disconnected: 'Disconnected',
   // Button to sync from an existing sync file
   selectSyncfile: 'Sync from a file…',
   // Button to create a new sync file
@@ -18,13 +24,39 @@ const m = defineMessages({
 const SyncAppBar = ({ onClickSelectSyncfile, onClickNewSyncfile }) => {
   const cx = useStyles()
   const { formatMessage: t } = useIntl()
+  const [currentConnection, setCurrentConnection] = useState(null)
+
+  // Check connection every 2 seconds
+  useEffect(() => {
+    const intervalCheck = setInterval(() => {
+      wifi
+        .getCurrentConnections()
+        .then((conn) => setCurrentConnection(conn && conn[0]))
+    }, 2000)
+    return () => clearInterval(intervalCheck)
+  }, [])
   return (
     <AppBar position='static' color='default' elevation={0} className={cx.root}>
       <Toolbar>
-        <Typography variant='h6' component='h1' className={cx.title}>
-          {t(m.title)}
-          <span className={cx.subtitle}>{' ' + t(m.subtitle)}</span>
-        </Typography>
+        <div className={cx.titleBar}>
+          <Typography variant='h6' component='h1' className={cx.title}>
+            {t(m.title)}
+          </Typography>
+          <span className={cx.wifi}>
+            {currentConnection ? (
+              <Wifi className={cx.wifiIcon} />
+            ) : (
+              <WifiOff className={cx.wifiIcon} />
+            )}
+            <Typography
+              variant='overline'
+              component='span'
+              className={cx.wifiName}
+            >
+              {currentConnection ? currentConnection.ssid : t(m.disconnected)}
+            </Typography>
+          </span>
+        </div>
         <Button
           onClick={onClickSelectSyncfile}
           color='inherit'
@@ -48,15 +80,31 @@ const SyncAppBar = ({ onClickSelectSyncfile, onClickNewSyncfile }) => {
 
 export default SyncAppBar
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: 'white'
   },
-  title: {
-    flex: 1
+  titleBar: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start'
   },
-  subtitle: {
-    fontWeight: 300
+  title: {
+    lineHeight: 1.2
+  },
+  wifi: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  wifiIcon: {
+    fontSize: '1rem',
+    position: 'relative',
+    top: -1 // visual alignment
+  },
+  wifiName: {
+    lineHeight: 1.2,
+    marginLeft: 5
   },
   actionArea: {},
   button: {
