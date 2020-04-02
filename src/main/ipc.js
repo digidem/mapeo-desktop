c08d1baa0b4b9ee648052846cafced9ffa88b607
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-04-01 21:49:57 -0700

    feat: Auto-update Mapeo when internet is available

--- src/main/ipc.js
+++ src/main/ipc.js
@@ -1,43 +1,90 @@
-var path = require('path')
-var { dialog, app, ipcMain } = require('electron')
+const path = require('path')
+const { dialog, app, ipcMain } = require('electron')
 
-var logger = require('../logger')
-var userConfig = require('./user-config')
-var i18n = require('./i18n')
+const updater = require('./auto-updater')
+const logger = require('../logger')
+const userConfig = require('./user-config')
+const i18n = require('./i18n')
 
 /**
- * Miscellaneous ipc calls that don't hit mapeo-core
+ * Miscellaneous ipcMain calls that don't hit mapeo-core
  */
-module.exports = function (win) {
-  var ipc = ipcMain
-
-  function ipcSend (...args) {
-    try {
-      win.webContents.send.apply(win.webContents, args)
-    } catch (e) {
-      logger.error('exception win.webContents.send', args, e.stack)
-    }
+module.exports = function (ipcSend) {
+  function onError (err) {
+    logger.error('[MAIN/IPC] error', err)
+    ipcSend('error', err.message, err.stack)
   }
 
-  ipc.on('get-user-data', function (event, type) {
+  updater.on('error', (err) => {
+    logger.error('[UPDATER] error', err)
+    ipcSend('update-status', 'update-error', err)
+    onError(err)
+  })
+
+  updater.updateDownloaded(function (updateInfo) {
+    logger.log('[UPDATER] update-downloaded', updateInfo)
+    ipcSend('update-status', 'update-downloaded', updateInfo)
+  })
+
+  updater.updateNotAvailable(function () {
+    logger.log('[UPDATER] update-not-available')
+    ipcSend('update-status', 'update-not-available', null)
+  })
+
+  updater.downloadProgress(function (updateInfo) {
+    logger.log('[UPDATER] update-progress', updateInfo)
+    /*
+      {
+        progress: {
+          total: 141164463,
+          delta: 1655048,
+          transferred: 11384326,
+          percent: 8.064583506402741,
+          bytesPerSecond: 2244544
+        }
+      }
+    */
+
+    ipcSend('update-status', 'update-progress', updateInfo)
+  })
+
+  updater.updateAvailable((updateInfo) => {
+    // version, files, path, sha512, releaseDate
+    logger.log('[UPDATER] update-available', updateInfo)
+    ipcSend('update-status', 'update-available', updateInfo)
+  })
+
+  ipcMain.on('download-update', function (event) {
+    updater.downloadUpdate()
+  })
+
+  ipcMain.on('check-for-updates', function (event) {
+    updater.checkForUpdates()
+  })
+
+  ipcMain.on('quit-and-install', function (event) {
+    updater.quitAndInstall()
+  })
+
+  ipcMain.on('get-user-data', function (event, type) {
     var data = userConfig.getSettings(type)
     if (!data) logger.debug('unhandled event', type)
     event.returnValue = data
   })
 
-  ipc.on('error', function (ev, message) {
-    ipcSend('error', message)
+  ipcMain.on('error', function (ev, message) {
+    onError(new Error(message))
   })
 
-  ipc.on('set-locale', function (ev, locale) {
+  ipcMain.on('set-locale', function (ev, locale) {
     app.translations = i18n.setLocale(locale)
   })
 
-  ipc.on('get-locale', function (ev) {
+  ipcMain.on('get-locale', function (ev) {
     ev.returnValue = i18n.locale
   })
 
-  ipc.on('import-example-presets', function (ev) {
+  ipcMain.on('import-example-presets', function (ev) {
     var filename = path.join(
       __dirname,
       '..',
@@ -51,7 +98,7 @@ module.exports = function (win) {
     })
   })
 
-  ipc.on('save-file', function () {
+  ipcMain.on('save-file', function () {
     var metadata = userConfig.getSettings('metadata')
     var ext = metadata ? metadata.dataset_id : 'mapeodata'
     dialog.showSaveDialog(
@@ -70,11 +117,11 @@ module.exports = function (win) {
 
     function onopen (filename) {
       if (typeof filename === 'undefined') return
-      win.webContents.send('select-file', filename)
+      ipcSend('select-file', filename)
     }
   })
 
-  ipc.on('open-file', function () {
+  ipcMain.on('open-file', function () {
     var metadata = userConfig.getSettings('metadata')
     var ext = metadata ? metadata.dataset_id : 'mapeodata'
     dialog.showOpenDialog(
@@ -95,20 +142,20 @@ module.exports = function (win) {
       if (typeof filenames === 'undefined') return
       if (filenames.length === 1) {
         var file = filenames[0]
-        win.webContents.send('select-file', file)
+        ipcSend('select-file', file)
       }
     }
   })
 
-  ipc.on('zoom-to-latlon-request', function (_, lon, lat) {
+  ipcMain.on('zoom-to-latlon-request', function (_, lon, lat) {
     ipcSend('zoom-to-latlon-response', [lon, lat])
   })
 
-  ipc.on('force-refresh-window', function () {
+  ipcMain.on('force-refresh-window', function () {
     ipcSend('force-refresh-window')
   })
 
-  ipc.on('refresh-window', function () {
+  ipcMain.on('refresh-window', function () {
     ipcSend('refresh-window')
   })
 }
---
+++ src/renderer/components/UpdaterView/index.js
@@ -0,0 +1,222 @@
+import React from 'react'
+import { LinearProgress, Typography, makeStyles } from '@material-ui/core'
+import { defineMessages, useIntl } from 'react-intl'
+import Button from '@material-ui/core/Button'
+import FormattedDuration from 'react-intl-formatted-duration'
+import ErrorIcon from '@material-ui/icons/Error'
+
+import electronIpc from '../../electron-ipc'
+
+export const STATES = {
+  IDLE: 0,
+  AVAILABLE: 1,
+  DOWNLOADING: 2,
+  PROGRESS: 3,
+  READY_FOR_RESTART: 4,
+  ERROR: 5
+}
+
+const m = defineMessages({
+  // Title on sync screen when searching for devices
+  updateAvailable: 'Update Available',
+  updateNotAvailable: 'Mapeo is up to date! You are on the latest version.',
+  downloadButtonText: 'Download now',
+  calculatingProgress: 'Estimating...',
+  downloadProgress: 'Download Progress',
+  restartMapeoText: 'An update to Mapeo has been downloaded. Restart Mapeo to update.',
+  restartMapeoButton: 'Restart Mapeo.',
+  errorTitle: 'Error',
+  errorMessage: 'There was an error and Mapeo could not update. Try again later.'
+})
+
+const errors = {
+  ERR_UPDATER_CHANNEL_FILE_NOT_FOUND: 'This channel does not have any updates'
+}
+
+export const MiniUpdaterView = ({ update }) => {
+  const cx = useStyles()
+  const { formatMessage: t } = useIntl()
+
+  var internal = function () {
+    switch (update.state) {
+      case STATES.AVAILABLE:
+        return <Typography>{t(m.updateAvailable)}</Typography>
+      case STATES.DOWNLOADING:
+        return <Typography>{t(m.calculatingProgress)}</Typography>
+      case STATES.PROGRESS:
+        return <DownloadProgressView cx={cx} percent={update.progress.percent} update={update} />
+      case STATES.READY_FOR_RESTART:
+        return <Typography>{t(m.restartMapeoButton)}</Typography>
+      case STATES.ERROR:
+        return <Typography style={{ color: 'red' }}>
+          {t(m.errorTitle)}
+        </Typography>
+      default: // STATES.IDLE, STATES.UPDATE_NOT_AVAILABLE:
+        return null
+    }
+  }
+
+  return <div className={cx.miniUpdaterView}>{internal()}</div>
+}
+
+export const UpdaterView = ({ update, setUpdate }) => {
+  const cx = useStyles()
+  const { formatMessage: t } = useIntl()
+
+  const downloadUpdateClick = () => {
+    setUpdate({
+      state: STATES.DOWNLOADING
+    })
+    electronIpc.downloadUpdate()
+  }
+
+  console.log(update)
+
+  var internal = function () {
+    switch (update.state) {
+      case STATES.AVAILABLE:
+        return (
+          <UpdateAvailableView cx={cx}>
+            <Button
+              onClick={downloadUpdateClick}
+              variant='contained'
+              size='large'
+              color='primary'>
+              {t(m.downloadButtonText)}
+            </Button>
+          </UpdateAvailableView>
+        )
+      case STATES.DOWNLOADING:
+        return <Typography>{t(m.calculatingProgress)}</Typography>
+      case STATES.PROGRESS:
+        return <DownloadProgressView cx={cx} percent={update.progress.percent} update={update} />
+      case STATES.READY_FOR_RESTART:
+        return <RestartView cx={cx} />
+      case STATES.UPDATE_NOT_AVAILABLE:
+        return <UpdateNotAvailableView cx={cx} />
+      case STATES.ERROR:
+        return <Typography>
+          <ErrorIcon
+            fontSize='inherit'
+            className={cx.icon}
+            style={{ color: 'red' }}
+          />
+          {t(m.errorMessage)}
+          {update.updateInfo.code}
+          {errors[update.updateInfo.code]}
+        </Typography>
+      default: // STATES.IDLE
+        return null
+    }
+  }
+
+  return (
+    <div className={cx.root}>
+      <div className={cx.searchingWrapper}>
+        <div className={cx.searching}>
+          {internal()}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const RestartView = ({ cx }) => {
+  const { formatMessage: t } = useIntl()
+  return (
+    <div className={cx.searchingText}>
+      <Typography gutterBottom variant='h2' className={cx.searchingTitle}>
+        {t(m.restartMapeoText)}
+      </Typography>
+    </div>
+  )
+}
+
+const DownloadProgressView = ({ cx, update, percent }) => {
+  /*
+      {
+        progress: {
+          total: 141164463,
+          delta: 1655048,
+          transferred: 11384326,
+          percent: 8.064583506402741,
+          bytesPerSecond: 2244544
+        }
+      }
+  */
+
+  const progress = update.progress
+  let estimatedTimeLeft
+
+  if (progress) {
+    estimatedTimeLeft = progress.total ? (progress.total - progress.transferred) / progress.bytesPerSecond : 0
+  }
+
+  return (
+    <div className={cx.searchingText}>
+      <LinearProgress variant='determinate' value={percent} color='secondary' />
+      <Typography>
+        {estimatedTimeLeft && <FormattedDuration seconds={estimatedTimeLeft} />}
+      </Typography>
+    </div>
+  )
+}
+
+const UpdateAvailableView = ({ cx, update, children }) => {
+  const { formatMessage: t } = useIntl()
+  return (
+    <div className={cx.searchingText}>
+      <Typography gutterBottom variant='h2' className={cx.searchingTitle}>
+        {t(m.updateAvailable)}
+      </Typography>
+      {children}
+    </div>
+  )
+}
+
+const UpdateNotAvailableView = ({ cx, update, downloadUpdateClick }) => {
+  const { formatMessage: t } = useIntl()
+  return (
+    <div className={cx.searchingText}>
+      <Typography gutterBottom variant='h2' className={cx.searchingTitle}>
+        {t(m.updateNotAvailable)}
+      </Typography>
+    </div>
+  )
+}
+
+const useStyles = makeStyles(theme => ({
+  searchingText: {
+    maxWidth: 300,
+    marginLeft: theme.spacing(2)
+  },
+  searchingWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    alignSelf: 'stretch',
+    justifySelf: 'stretch'
+  },
+  searching: {
+    color: '#00052b',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  },
+  searchingTitle: {
+    fontSize: '2em',
+    fontWeight: 400
+  },
+  root: {
+    height: '100%',
+    width: '100%',
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#F5F5F5'
+  },
+  icon: {
+    fontSize: 48
+  }
+}))
