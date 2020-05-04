b30e94b99b37194069dde8a72932b049620198b9
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-05-04 11:04:54 +0200

    chore(DX): Fix storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,7 +6,9 @@ const alias = {
     __dirname,
     '__mocks__/fs-write-stream-atomic.js'
   ),
-  electron: path.resolve(__dirname, '__mocks__/electron.js')
+  electron: path.resolve(__dirname, '__mocks__/electron.js'),
+  'node-wifi': path.resolve(__dirname, '__mocks__/node-wifi.js'),
+  perf_hooks: path.resolve(__dirname, '__mocks__/perf_hooks.js')
 }
 
 module.exports = async ({ config }) => {
