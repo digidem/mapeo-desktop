0333177fedeb67f9d9316fd99150a4ba2c0a805e
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-07-01 19:19:15 +0100

    Extract other bits of MapFilter

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -11,22 +11,10 @@ import Home from './components/Home'
 
 const initialLocale = ipcRenderer.sendSync('get-locale') // navigator.language.slice(0, 2)
 
-const mdMsgs = {
+const msgs = {
   en: require('../../translations/en.json'),
   es: require('../../translations/es.json'),
   pt: require('../../translations/pt.json')
-}
-
-const mfMsgs = {
-  en: require('react-mapfilter/translations/en.json'),
-  es: require('react-mapfilter/translations/es.json'),
-  pt: require('react-mapfilter/translations/pt.json')
-}
-
-const allMsgs = {
-  en: { ...mdMsgs.en, ...mfMsgs.en },
-  es: { ...mdMsgs.es, ...mfMsgs.es },
-  pt: { ...mdMsgs.pt, ...mfMsgs.pt }
 }
 
 const App = () => {
@@ -46,7 +34,7 @@ const App = () => {
     <StylesProvider injectFirst>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <IntlProvider locale={locale} messages={allMsgs[locale]}>
+        <IntlProvider locale={locale} messages={msgs[locale]}>
           <Home onSelectLanguage={handleLanguageChange} />
         </IntlProvider>
       </ThemeProvider>
