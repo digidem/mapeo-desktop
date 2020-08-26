394bdfe7881a7a35116fdaf2069c739ccf067a26
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-08-25 17:34:39 -0700

    feat: cache maps on backend

---
+++ src/background/maps.js
@@ -0,0 +1,48 @@
+var Router = require('routes')
+var mapStream = require('mapbox-map-image-stream')
+var Core = require('@mapeo/core')
+
+var fileCacheMiddleware = require('./cache')
+var logger = require('../logger')
+var errors = Core.errors
+
+var token = 'pk.eyJ1IjoiZ21hY2xlbm5hbiIsImEiOiJSaWVtd2lRIn0.ASYMZE2HhwkAw4Vt7SavEg'
+var style = 'mapbox://styles/mapbox/outdoors-v10'
+
+var router = Router()
+
+function getAsset (req, res, params) {
+  const { bbox, width, height, dpi } = params
+  return {
+    contentType: 'image/png',
+    contentLength: 1000,
+    buffer: render({
+      token: token,
+      style: style,
+      bbox: JSON.parse(bbox),
+      width: parseInt(width),
+      height: parseInt(height),
+      pixelRatio: parseInt(dpi)
+    })
+  }
+}
+
+var cache = fileCacheMiddleware(getAsset, { maxSize: 10 * 1024 * 1024 * 1024 })
+
+router.addRoute('/map/:bbox/:width/:height/x:dpi.png', function (req, res, params) {
+  cache(req, res, params, (err) => {
+    if (err) {
+      logger.error(err)
+      errors.send(res, err)
+    }
+  })
+})
+
+module.exports = {
+  handle: (req, res) => {
+    var route = router.match(req.url)
+    if (!route) return false
+    route.fn.apply(null, [req, res, route.params, route.splats])
+    return true
+  }
+}
