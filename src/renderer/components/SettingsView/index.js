4729f74e2b3898f295c9841133e9b6b4f5232d6d
lightlii <32508751+lightlii@users.noreply.github.com>
2023-07-14 13:46:58 +0200

    chore: more improved typing

--- src/renderer/components/SettingsView/index.js
+++ src/renderer/components/SettingsView/index.js
@@ -7,16 +7,17 @@ import styled from 'styled-components'
 
 const m = defineMessages({
   aboutMapeo: 'About Mapeo',
-  aboutMapeoSubtitle: 'Version and build number'
+  aboutMapeoSubtitle: 'Version and build number',
 })
 
-const tabs = /** @typedef {const} */ [
+const tabs = [
   {
+    /** @type {import('./SettingsMenu').tabId} */
     tabId: 'AboutMapeo',
     icon: InfoIcon,
     label: m.aboutMapeo,
-    subtitle: m.aboutMapeoSubtitle
-  }
+    subtitle: m.aboutMapeoSubtitle,
+  },
 ]
 
 export const SettingsView = () => {
@@ -26,12 +27,7 @@ export const SettingsView = () => {
 
   return (
     <Container>
-      <SettingsMenu
-        tabs={tabs}
-        currentTab={menuItem}
-        onTabChange={setMenuItem}
-      />
-
+      <SettingsMenu tabs={tabs} currentTab={menuItem} onTabChange={setMenuItem} />
       {menuItem === 'AboutMapeo' && <AboutMapeoMenu />}
     </Container>
   )
