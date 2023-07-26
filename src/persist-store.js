bee804640155018a887c44fc7a2961849fcbe1f6
lightlii <32508751+lightlii@users.noreply.github.com>
2023-07-26 14:21:37 +0200

    WIP: add schema

--- src/persist-store.js
+++ src/persist-store.js
@@ -1,5 +1,71 @@
 const Store = require('electron-store')
 
-const persistedStore = new Store()
+const schema = {
+  'experiments-flags': {
+    type: 'object',
+    properties: {
+      state: {
+        type: 'object',
+        properties: {
+          backgroundMaps: { type: 'boolean' },
+          storeVersion: { type: 'string' }
+        }
+      },
+      version: { type: 'number' }
+    }
+  },
+  'background-maps': {
+    type: 'object',
+    properties: {
+      state: {
+        type: 'object',
+        properties: {
+          mapStyle: { type: 'string' },
+          storeVersion: { type: 'string' }
+        }
+      },
+      version: { type: 'number' }
+    }
+  },
+  ui: {
+    type: 'object',
+    properties: {
+      state: {
+        type: 'object',
+        properties: {
+          tabIndex: { type: 'number' }
+        }
+      },
+      version: { type: 'number' }
+    }
+  },
+  state: {
+    type: 'object',
+    properties: {
+      maximize: { type: 'boolean' },
+      fullscreen: { type: 'boolean' },
+      defaultWidth: { type: 'number' },
+      defaultHeight: { type: 'number' },
+      isMaximized: true,
+      show: { type: 'boolean' },
+      x: { type: 'number' },
+      y: { type: 'number' },
+      width: { type: 'number' },
+      height: { type: 'number' },
+      isFullScreen: { type: 'boolean' },
+      displayBounds: {
+        type: 'object',
+        properties: {
+          x: { type: 'number' },
+          y: { type: 'number' },
+          width: { type: 'number' },
+          height: { type: 'number' }
+        }
+      }
+    }
+  }
+}
+
+const persistedStore = new Store({ schema })
 
 module.exports = persistedStore
