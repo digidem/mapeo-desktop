c472c58acbf7d18765a73591e7ef6650ddc1c30c
Gregor MacLennan <gmaclennan@awana.digital>
2025-03-04 12:29:40 +0900

    catch errors when adding field stats

--- src/renderer/components/MapFilter/lib/data_analysis/statistics.js
+++ src/renderer/components/MapFilter/lib/data_analysis/statistics.js
@@ -76,7 +76,11 @@ function addItemStats (item: JSONObject = {}, stats: Statistics) {
   flatObjectEntries(item).forEach(function ([path, value]) {
     const key = JSON.stringify(path)
     if (!stats[key]) stats[key] = defaultStats()
-    addFieldStats(value, stats[key])
+    try {
+      addFieldStats(value, stats[key])
+    } catch (e) {
+      console.error('Error adding field stats for value %s', value, e)
+    }
   })
 }
 
