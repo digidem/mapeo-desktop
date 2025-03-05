c511d5252cee0279fd863abe12d83fe50710d072
Gregor MacLennan <gmaclennan@awana.digital>
2025-03-05 11:17:07 +0900

    fix notarization

--- bin/notarize.js
+++ bin/notarize.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { notarize } = require('electron-notarize')
+const { notarize } = require('@electron/notarize')
 const { log } = require('builder-util')
 
 exports.default = async function notarizing (context) {
@@ -8,13 +8,13 @@ exports.default = async function notarizing (context) {
     appOutDir,
     packager: { appInfo }
   } = context
-  const { APPLE_ID, APPLE_ID_PASSWORD } = process.env
+  const { APPLE_ID, APPLE_ID_PASSWORD, APPLE_TEAM_ID } = process.env
 
   if (electronPlatformName !== 'darwin') {
     return
   }
 
-  if (!APPLE_ID || !APPLE_ID_PASSWORD) {
+  if (!APPLE_ID || !APPLE_ID_PASSWORD || !APPLE_TEAM_ID) {
     log.warn(
       'Missing APPLE_ID and/or APPLE_ID_PASSWORD in environment, skipping notarizing'
     )
@@ -22,13 +22,16 @@ exports.default = async function notarizing (context) {
   }
 
   const notarizeConfig = {
-    appBundleId: appInfo.macBundleIdentifier,
     appPath: `${appOutDir}/${appInfo.productFilename}.app`,
     appleId: APPLE_ID,
-    appleIdPassword: APPLE_ID_PASSWORD
+    appleIdPassword: APPLE_ID_PASSWORD,
+    teamId: APPLE_TEAM_ID
   }
 
-  log.info({ ...notarizeConfig, appleIdPassword: '*****' }, 'notarizing')
+  log.info(
+    { ...notarizeConfig, appleIdPassword: '*****', teamId: '*****' },
+    'notarizing'
+  )
 
   return notarize(notarizeConfig)
 }
